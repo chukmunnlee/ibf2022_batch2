4f2aa4edca9b7502a6ab0805312ea8d060a50416
Chuk <chukmunnlee@gmail.com>
2023-03-29 10:18:00 +0800

    day28 AM pre tea

---
+++ day28/src/main/java/ibf2022/batch2/paf/day28/Day28Application.java
@@ -0,0 +1,27 @@
+package ibf2022.batch2.paf.day28;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import ibf2022.batch2.paf.day28.repositories.TvShowRepository;
+
+@SpringBootApplication
+public class Day28Application implements CommandLineRunner {
+
+	@Autowired 
+	private TvShowRepository tvshowRepo;
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day28Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) {
+
+		//tvshowRepo.findTvShowsByType("Animation");
+		tvshowRepo.groupShowsByTimezone();
+	}
+
+}
---
+++ day28/src/main/java/ibf2022/batch2/paf/day28/repositories/TvShowRepository.java
@@ -0,0 +1,65 @@
+package ibf2022.batch2.paf.day28.repositories;
+
+import org.bson.Document;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
+import org.springframework.data.mongodb.core.aggregation.GroupOperation;
+import org.springframework.data.mongodb.core.aggregation.MatchOperation;
+import org.springframework.data.mongodb.core.aggregation.ProjectionOperation;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class TvShowRepository {
+
+	public static final String C_TVSHOWS = "tvshows";
+
+	@Autowired
+	private MongoTemplate template;
+
+	/*
+	 * db.tvshows.aggregate([
+	 * 	{ $match: { type: 'a type' } }
+	 * ])
+	 */
+	public void findTvShowsByType(String type) {
+
+		// stages
+		MatchOperation matchType = Aggregation.match(
+				Criteria.where("type").regex(type, "i"));
+
+		ProjectionOperation selectFields = Aggregation.project("id", "name", "summary")
+				.andExclude("_id");
+
+		// create the pipeline from stages
+		Aggregation pipeline = Aggregation.newAggregation(matchType, selectFields);
+
+		// perform the query
+		AggregationResults<Document> results = template.aggregate(
+				pipeline, C_TVSHOWS, Document.class);
+
+		printIt(results);
+	}
+
+	public void groupShowsByTimezone() {
+
+		// Stage
+		GroupOperation tzGroup = Aggregation.group("network.country.timezone")
+			.count().as("total_shows")
+			.push("name").as("titles");
+
+		Aggregation pipeline = Aggregation.newAggregation(tzGroup);
+
+		AggregationResults<Document> results = template.aggregate(
+				pipeline, C_TVSHOWS, Document.class);
+
+		printIt(results);
+	}
+
+	private void printIt(AggregationResults<Document> results) {
+		for (Document d: results)
+			System.out.printf(">>> %s\n", d.toJson());
+	}
+}
