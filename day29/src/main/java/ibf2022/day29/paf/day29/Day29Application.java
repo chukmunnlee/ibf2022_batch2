3979e53b258cbfee0c9ff27c230da1783e765027
chukmunnlee <chukmunnlee@gmail.com>
2023-03-30 08:47:49 +0800

    renamed day29

---
+++ day29/src/main/java/ibf2022/day29/paf/day29/Day29Application.java
@@ -0,0 +1,13 @@
+package ibf2022.day29.paf.day29;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day29Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day29Application.class, args);
+	}
+
+}
---
+++ day29/src/test/java/ibf2022/day29/paf/day29/Day29ApplicationTests.java
@@ -0,0 +1,13 @@
+package ibf2022.day29.paf.day29;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day29ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
