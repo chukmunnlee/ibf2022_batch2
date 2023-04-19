5528a61578285ed433d02b166309993cf4230ff3
Chuk <chukmunnlee@gmail.com>
2023-04-19 12:05:41 +0800

    day33 AM demos

--- day33/src/app/app.component.ts
+++ day33/src/app/app.component.ts
@@ -1,19 +1,53 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { Friend } from './models';
+import { FriendsComponent } from './components/friends.component';
+import { FriendsListComponent } from './components/friends-list.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
+
+  @ViewChild(FriendsComponent)
+  friendComp!: FriendsComponent
+
+  @ViewChild(FriendsListComponent)
+  friendsList!: FriendsListComponent
 
   selectedFriend!: Friend;
+
+  updateMode = false
 
   friends: Friend[] = [
     { name: 'barney', email: 'barney@gmail.com', phone: '23456' },
     { name: 'betty', email: 'betty@gmail.com', phone: '23456' },
   ]
+
+  ngOnInit(): void {
+    console.info('>> onInit: firendComp: ', this.friendComp)
+  }
+
+  ngAfterViewInit(): void {
+    console.info('>> onAfterViewInit: firendComp: ', this.friendComp)
+    // this.friendsList.onSelectedFriend.subscribe(
+    //   f => {
+    //     console.info('in subscribe: ', f)
+    //     this.selection(f)
+    //   }
+    // )
+  }
+
+  unFriend() {
+    let idx = this.friends.findIndex(f => f.name == this.selectedFriend.name)
+    if (idx == -1)
+      return
+
+    this.friends.splice(idx, 1)
+    console.info('>>> deleting: ', this.friendComp.value)
+    this.friendComp.clear()
+  }
 
   process(friend: Friend) {
     let idx = this.friends.findIndex(f => f.name == friend.name)
@@ -28,6 +62,8 @@ export class AppComponent {
     console.info(">>> selected friend: ", name)
     const fr = this.friends.find(f => f.name == name)
     // @ts-ignore
-    this.selectedFriend = fr
+    //this.selectedFriend = fr
+    this.friendComp.value = fr
+    this.updateMode = true
   }
 }
--- day33/src/app/components/friends.component.ts
+++ day33/src/app/components/friends.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Friend } from '../models';
-import { Subject } from 'rxjs';
+import { Subject, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-friends',
@@ -18,17 +18,54 @@ export class FriendsComponent implements OnInit, OnChanges {
 
   form!: FormGroup
 
+  get value(): Friend {
+    return this.form.value as Friend
+  }
+  set value(f: Friend) {
+    console.info('>>> set value: ', f)
+    this.initForm()
+  }
+
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
-    this.form = this.createForm(this.friend)
+    this.initForm(this.friend)
     console.info('friend: ', this.friend)
   }
 
   ngOnChanges(changes: SimpleChanges): void {
     const fr = changes['friend'].currentValue as Friend
+    console.info('******* changes: ', changes)
     this.form = this.createForm(fr)
   }
+
+  initForm(friend: Friend | null = null) {
+    this.form = this.createForm(friend)
+    /*
+    this.form.statusChanges.subscribe(
+      v => {
+        console.info('status changes: ', v)
+      }
+    )
+    */
+    this.form.valueChanges
+      .pipe(
+        debounceTime(300)
+      )
+      .subscribe(
+      v => {
+        console.info('value changes: ', v['name'])
+      }
+    )
+  }
+
+  clear() {
+    this.form = this.createForm()
+  }
+
+  // value(): Friend {
+  //   return this.form.value as Friend
+  // }
 
   processForm() {
     const fr: Friend = this.form.value
@@ -37,7 +74,8 @@ export class FriendsComponent implements OnInit, OnChanges {
     this.form.reset()
   }
 
-  private createForm(friend: Friend): FormGroup {
+  private createForm(friend: Friend | null = null): FormGroup {
+    console.info('>>> create form: ', friend)
     return this.fb.group({
       name: this.fb.control(friend? friend.name: '', [ Validators.required ]),
       phone: this.fb.control(friend? friend.phone: '', [ Validators.required ]),
